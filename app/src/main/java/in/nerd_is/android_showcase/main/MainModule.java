3d3b386abd83a55eaeb817d83075c277c2c03c83
Zheng Xuqiang <zhengxuqiang@qiaobutang.com>
2016-09-23 18:24:40 +0800

    temp commit

--- app/src/main/java/in/nerd_is/android_showcase/main/MainModule.java
+++ app/src/main/java/in/nerd_is/android_showcase/main/MainModule.java
@@ -1,10 +1,22 @@
 package in.nerd_is.android_showcase.main;
 
 import dagger.Module;
+import dagger.Provides;
 
 /**
  * Created by Xuqiang ZHENG on 2016/9/20.
  */
 @Module
 public class MainModule {
+
+    private MainContract.View view;
+
+    public MainModule(MainContract.View view) {
+        this.view = view;
+    }
+
+    @Provides
+    public MainContract.View provideVeiw() {
+        return view;
+    }
 }
