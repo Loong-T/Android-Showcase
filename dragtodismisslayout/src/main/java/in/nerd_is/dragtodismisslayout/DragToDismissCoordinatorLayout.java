96fcd4c65612cbdf932047c5a1d7747a2618f9b7
Xuqiang ZHENG <talentloong@gmail.com>
2017-04-19 16:59:30 +0800

    refactor DragToDismissLayout

--- dragtodismisslayout/src/main/java/in/nerd_is/dragtodismisslayout/DragToDismissCoordinatorLayout.java
+++ dragtodismisslayout/src/main/java/in/nerd_is/dragtodismisslayout/DragToDismissCoordinatorLayout.java
@@ -17,31 +17,17 @@
 package in.nerd_is.dragtodismisslayout;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Xuqiang ZHENG on 2017/4/7.
  */
 public class DragToDismissCoordinatorLayout extends CoordinatorLayout {
 
-    private float dragDismissDistance = Float.MAX_VALUE;
-    private float dragDismissFraction = -1f;
-    private float dragDismissScale = 1f;
-    private boolean shouldScale = false;
-    private float dragElasticity = 0.618f;
-
-    private float totalDrag;
-    private boolean draggingDown = false;
-    private boolean draggingUp = false;
-
-    private List<DragToDismissCallback> callbacks;
+    private DragToDismissHelper helper;
 
     public DragToDismissCoordinatorLayout(Context context) {
         this(context, null, 0);
@@ -54,20 +40,7 @@ public class DragToDismissCoordinatorLayout extends CoordinatorLayout {
     public DragToDismissCoordinatorLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        final TypedArray a = getContext().obtainStyledAttributes(
-                attrs, R.styleable.DragToDismissLayout, 0, 0);
-
-        dragDismissDistance = a.getDimension(R.styleable
-                .DragToDismissLayout_dragDismissDistance, dragDismissDistance);
-        dragDismissFraction = a.getFloat(R.styleable
-                .DragToDismissLayout_dragDismissFraction, dragDismissFraction);
-        dragDismissScale = a.getFloat(R.styleable
-                .DragToDismissLayout_dragDismissScale, dragDismissScale);
-        shouldScale = dragDismissScale != 1f;
-        dragElasticity = a.getFloat(R.styleable
-                .DragToDismissLayout_dragElasticity, dragElasticity);
-
-        a.recycle();
+        helper = DragToDismissHelper.create(context, attrs, this);
     }
 
     @Override
@@ -79,8 +52,8 @@ public class DragToDismissCoordinatorLayout extends CoordinatorLayout {
     @Override
     public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
         super.onNestedPreScroll(target, dx, dy, consumed);
-        if (draggingDown && dy > 0 || draggingUp && dy < 0) {
-            dragScale(dy);
+        if (helper.draggingDown && dy > 0 || helper.draggingUp && dy < 0) {
+            helper.dragScale(dy);
             consumed[1] = dy;
         }
     }
@@ -89,128 +62,26 @@ public class DragToDismissCoordinatorLayout extends CoordinatorLayout {
     public void onNestedScroll(View target, int dxConsumed, int dyConsumed,
                                int dxUnconsumed, int dyUnconsumed) {
         super.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
-        dragScale(dyUnconsumed);
+        helper.dragScale(dyUnconsumed);
     }
 
     @Override
     public void onStopNestedScroll(View child) {
         super.onStopNestedScroll(child);
-
-        if (Math.abs(totalDrag) >= dragDismissDistance) {
-            dispatchDismissCallback();
-        } else { // settle back to natural position
-            animate().translationY(0f)
-                    .scaleX(1f)
-                    .scaleY(1f)
-                    .setDuration(200L)
-                    .start();
-            totalDrag = 0;
-            draggingUp = false;
-            draggingDown = false;
-            dispatchDragCallback(0f, 0f, 0f, 0f);
-        }
+        helper.finishOrCancel();
     }
 
     public void addListener(DragToDismissCallback listener) {
-        if (callbacks == null) {
-            callbacks = new ArrayList<>();
-        }
-        callbacks.add(listener);
+        helper.addListener(listener);
     }
 
     public void removeListener(DragToDismissCallback listener) {
-        if (callbacks != null && callbacks.size() > 0) {
-            callbacks.remove(listener);
-        }
+        helper.removeListener(listener);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        if (dragDismissFraction > 0f) {
-            dragDismissDistance = h * dragDismissFraction;
-        }
+        helper.onHeightChanged(h);
     }
-
-    private void dragScale(int scroll) {
-        if (scroll == 0) {
-            return;
-        }
-
-        totalDrag += scroll;
-        float absTotalDrag = Math.abs(totalDrag);
-
-        // track the direction & set the pivot point for scaling
-        // don't double track i.e. if start dragging down and then reverse, keep tracking as
-        // dragging down until they reach the 'natural' position
-        if (!draggingUp && !draggingDown) {
-            if (scroll < 0) {
-                draggingDown = true;
-                if (shouldScale) {
-                    setPivotY(getHeight());
-                }
-            } else {
-                draggingUp = true;
-                if (shouldScale) {
-                    setPivotY(0f);
-                }
-            }
-        }
-        // how far have we dragged relative to the distance to perform a dismiss
-        // (0–1 where 1 = dismiss distance). Decreasing logarithmically as we approach the limit
-        float dragFraction = (float) MathUtils.log2(1 + (absTotalDrag / dragDismissDistance));
-
-        // calculate the desired translation given the drag fraction
-        float dragTo = dragFraction * dragDismissDistance * dragElasticity;
-
-        if (draggingUp) {
-            // as we use the absolute magnitude when calculating the drag fraction, need to
-            // re-apply the drag direction
-            dragTo *= -1;
-        }
-        setTranslationY(dragTo);
-
-        if (shouldScale && absTotalDrag > dragDismissDistance) {
-            // 1 = log2(1 + dragDismissDistance / dragDismissDistance)
-            final float scale = 1 - ((1 - dragDismissScale) * (dragFraction - 1));
-            setScaleX(scale);
-            setScaleY(scale);
-        }
-
-        // if we've reversed direction and gone past the settle point then clear the flags to
-        // allow the list to get the scroll events & reset any transforms
-        if ((draggingDown && totalDrag >= 0)
-                || (draggingUp && totalDrag <= 0)) {
-            totalDrag = 0;
-            dragTo = 0;
-            dragFraction = 0;
-            draggingDown = false;
-            draggingUp = false;
-            setTranslationY(0f);
-            setScaleX(1f);
-            setScaleY(1f);
-        }
-
-        dispatchDragCallback(dragFraction, dragTo,
-                Math.min(1f, Math.abs(totalDrag) / dragDismissDistance), totalDrag);
-    }
-
-    private void dispatchDragCallback(float elasticOffset, float elasticOffsetPixels,
-                                      float rawOffset, float rawOffsetPixels) {
-        if (callbacks != null && !callbacks.isEmpty()) {
-            for (DragToDismissCallback callback : callbacks) {
-                callback.onDrag(elasticOffset, elasticOffsetPixels,
-                        rawOffset, rawOffsetPixels);
-            }
-        }
-    }
-
-    private void dispatchDismissCallback() {
-        if (callbacks != null && !callbacks.isEmpty()) {
-            for (DragToDismissCallback callback : callbacks) {
-                callback.onDragDismissed();
-            }
-        }
-    }
-
 }
